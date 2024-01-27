15bb76cc3f1ffc429563ad5a4c86a3d83de69732
brokeboy <ras0087@ya.ru>
2024-01-28 01:46:14 +0700

    маленькие правки

--- homeworks/loops/6.py
+++ homeworks/loops/6.py
@@ -3,6 +3,11 @@ res = 0
 while price != 0:
     res += price
     price = int(input())
-while res > 0
 
+if res%2 == 0:
+    while res%2==0:
+        res /= 2
+    print(f'the result is {res}')
+else:
+    print(f'the result is {res*0.85}')
 
